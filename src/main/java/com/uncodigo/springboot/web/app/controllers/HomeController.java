820455db5c4e117bd9017f5e7f0be3b2674a40c6
felipejoq <felipejofreq@gmail.com>
2022-12-07 17:22:10 -0300

    Manejando redireccionamientos usando redirect pero también forward

---
+++ src/main/java/com/uncodigo/springboot/web/app/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.uncodigo.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		//return "redirect:/app"; // -> Resetea la petición cambiando la url donde hace get.
+		return "forward:/app"; // -> Solo cambia la vista, solo rutas internas.
+	}
+	
+}
